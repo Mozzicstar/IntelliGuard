98f996b6b3dbc02259760b0add17eaad0862d856
Mozzicstar <146447403+Mozzicstar@users.noreply.github.com>
2025-08-21 07:40:42 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def hello():
+    return 'Hello World!'
 
-
+# This is required for Vercel
 if __name__ == '__main__':
     app.run()
